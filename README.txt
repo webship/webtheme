27c228d04b1e23ba0347f0adf1dd9619a11a529f
Rajab Natshah <rajabn@gmail.com>
2020-04-11 04:22:43 +0300

    Issue #3126764: Start an 8.2.x branch for Webtheme

--- README.txt
+++
@@ -1,4 +0,0 @@
-# Webtheme
----------------------
-
- * Introduction
---
+++ src/js/webtheme.script.js
@@ -0,0 +1,14 @@
+import 'popper.js';
+import 'bootstrap';
+
+(function () {
+
+  'use strict';
+
+  Drupal.behaviors.webthemeScript = {
+    attach: function (context) {
+
+    }
+  };
+
+})(jQuery, Drupal);
